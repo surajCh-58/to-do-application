c272ba2acac5035774da57092e3d1a3813757393
suraj <surajchamlagain058@gmail.com>
2026-06-28 18:55:41 +0545

    jazzmin added

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Task
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display=('id','name','created_at')
+    search_fields=('name',)
